8bc4e74e63a6d7978d16495fbb77ee83f6ef142f
lradeck <leon.radeck@web.de>
2023-09-14 15:08:38 +0200

    Create util_posix.sh

---
+++ util_posix.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+check_connection() {
+  host=$1
+  port=$2
+  echo "Host: $host"
+  echo "Port: $port"
+  echo "Connecting to http://$host:$port..."
+  timeout 3m sh -c "until curl -s http://$host:$port; do sleep 2; done"
+  if [ $? -ne 0 ]; then
+    echo "Timed out."
+    return 1
+  else
+    echo "Connected."
+    return 0
+  fi
+}
+
+check_database() {
+  echo "User: $1"
+  echo "Pass: $2"
+  echo "Host: $3"
+  echo "Port: $4"
+  echo "Db: $5"
+  echo "Trying to connect to database..."
+  timeout 3m sh -c "until psql postgresql://$1:$2@$3:$4/$5 -c '\q' > /dev/null; do sleep 2; done"
+  if [ $? -eq 0 ]; then
+    echo "Connected to database"
+    npm start
+  else
+    echo "Timed out."
+    echo "Exiting container."
+  fi
+}
